176ba08ded85b24e7b64e88a138901767c3cbef4
jreinke428 <93751082+jreinke428@users.noreply.github.com>
2023-09-22 08:51:48 -0500

    settings page and router layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 //
+import 'package:first_flutter_app/layout.dart';
+import 'package:first_flutter_app/login.dart';
+import 'settings.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 //
 void main() => runApp(const MyApp());
 
@@ -14,135 +15,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'eMission',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('eMission Application'),
-          backgroundColor: const Color.fromRGBO(124, 184, 22, 40),
-        ),
-        body:  Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/pexels-robert-so-18127674-2.jpg"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Column(
-            children: <Widget>[
-              SvgPicture.asset('assets/images/eMission_logo.svg', height: 100),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-                decoration: const BoxDecoration( 
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  color:  Color.fromRGBO(238, 230, 231, 0.877),
-                ), 
-                // Login Form
-                child: const MyCustomForm(),
-                // Text Button
-              ),
-            ],
-          )
-
-        ),
-      ),
+      initialRoute: 'home',
+      routes: {
+        'login': (context) => const Layout(body: Login(), appBar: false, bottomBar: false),
+        'home': (context) => const Layout(body: Settings(), pageIndex: 1,),
+        'leaderboard': (context) => const Layout(body: Settings(), pageIndex: 0,),
+        'settings': (context) => const Layout(body: Settings(), pageIndex: 2,)
+      },
     );
   }
 }
-
-
-
-/* */
-class  MyCustomForm extends StatelessWidget {
-  const MyCustomForm({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'email',
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'username',
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'display name',
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'password',
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'confirm password',
-            ),
-          ),
-        ),
-        Container(
-          alignment: Alignment.bottomCenter,
-          padding: const EdgeInsets.only(bottom: 10),
-          child: SizedBox(
-            height: 30,
-            width: 250,
-            child: TextButton(
-            style: TextButton.styleFrom(
-              foregroundColor:const Color.fromARGB(244, 0, 0, 0),
-              backgroundColor: const Color.fromARGB(244, 244, 248, 6),
-            ),
-            onPressed: () { },
-            child: const Text('Submit')),
-          ),
-        ),        
-      ],
-    );
-  }
-}
-
-
-
-
-
-/* */
-class HomePage extends StatelessWidget {
-    const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
-      body: Center(child: ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        child: const Text('BUTTON')
-      ))
-    );
-  }
-}
-
