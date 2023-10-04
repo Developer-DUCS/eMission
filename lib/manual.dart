64928e67ee8823c167c63ae8fca99a160a890f9d
Jacob <jhutchinson@drury.edu>
2023-10-04 09:54:47 -0500

    10/4 changes

--- lib/manual.dart
+++ lib/manual.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:dropdown_button2/dropdown_button2.dart';
 
-class Manual extends StatelessWidget {
-  const Manual({super.key});
+class Manual extends StatefulWidget {
+  const Manual({Key? key});
 
+  @override
+  _ManualState createState() => _ManualState();
+}
+
+class _ManualState extends State<Manual> {
+  List<String> items = ['Car 1', 'Truck 2', 'Van 3', 'Motorcycle 4'];
+  String selectedItem = 'Car 1';
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +18,30 @@ class Manual extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: Column(
-              children: 
-              
-              <Widget>[
+              children: <Widget>[
+
+                Expanded(
+                  child: Container(
+                    color: Color.fromRGBO(124, 184, 22, 1),
+                    //padding: EdgeInsets.all(16.0),
+                    child: DropdownButton<String>(
+                      value: selectedItem,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedItem = newValue ?? items[0]; // Provide a default value if newValue is null
+                        });
+                      },
+                      menuMaxHeight: 100,
+                      items: items.map((String item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+
                 Expanded(
                   child: Container(
                     color: Color.fromRGBO(124, 184, 22, 1),
@@ -32,22 +59,13 @@ class Manual extends StatelessWidget {
     );
   }
 
-  Widget progressBar() {
-    return Container(
-      child: LinearProgressIndicator(
-        value: 0.4,
-      ),
-    );
-  }
-
   Widget textBox() {
     return TextField(
       decoration: InputDecoration(
         border: OutlineInputBorder(),
         hintText: 'Enter a search term',
       ),
-      keyboardType: TextInputType.number
+      keyboardType: TextInputType.number,
     );
   }
-
 }
