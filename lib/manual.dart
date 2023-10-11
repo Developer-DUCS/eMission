98ec2e66b35a18c51efd24e88a109f6ae43282b9
Jacob Hutchinson <jhutchinson@drury.edu>
2023-10-10 20:44:52 -0500

    fixed UI and added submit button

--- lib/manual.dart
+++ lib/manual.dart
@@ -20,24 +20,27 @@ class _ManualState extends State<Manual> {
             child: Column(
               children: <Widget>[
 
-                Expanded(
-                  child: Container(
-                    color: Color.fromRGBO(124, 184, 22, 1),
-                    //padding: EdgeInsets.all(16.0),
-                    child: DropdownButton<String>(
-                      value: selectedItem,
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          selectedItem = newValue ?? items[0]; // Provide a default value if newValue is null
-                        });
-                      },
-                      menuMaxHeight: 100,
-                      items: items.map((String item) {
-                        return DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(item),
-                        );
-                      }).toList(),
+                Container(
+                  color: Color.fromRGBO(124, 184, 22, 1),
+                  padding: EdgeInsets.only(top: 200.0),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      //width: 100,
+                      child: DropdownButton<String>(
+                        value: selectedItem,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedItem = newValue ?? items[0];
+                          });
+                        },
+                        items: items.map((String item) {
+                          return DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(item),
+                          );
+                        }).toList(),
+                      ),
                     ),
                   ),
                 ),
@@ -51,6 +54,15 @@ class _ManualState extends State<Manual> {
                   ),
                 ),
 
+                Expanded(
+                  child: Container(
+                    color: Color.fromRGBO(124, 184, 22, 1),
+                    child: Center(
+                      child: submitButton(),
+                    ),
+                  ),
+                ),
+
               ],
             ),
           ),
@@ -60,12 +72,34 @@ class _ManualState extends State<Manual> {
   }
 
   Widget textBox() {
-    return TextField(
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        hintText: 'Enter a search term',
+    return Container(
+      width: 200, // Set the desired width here
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'Enter Your Total Milage',
+        ),
+        keyboardType: TextInputType.number,
       ),
-      keyboardType: TextInputType.number,
     );
   }
+
+  Widget submitButton() {
+    return Container(
+      alignment: Alignment.topCenter,
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        height: 30,
+        width: 250,
+        child: TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor:const Color.fromARGB(244, 0, 0, 0),
+            backgroundColor: const Color.fromARGB(244, 244, 248, 6),
+          ),
+          onPressed: () { Navigator.pushNamed(context, 'manual'); },
+        child: const Text('Submit')),
+      ),
+    ); 
+  }
+
 }
