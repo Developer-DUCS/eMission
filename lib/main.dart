402503024066b898aec38b13c8d116a26eceec42
jalikat3 <78003140+jalikat3@users.noreply.github.com>
2023-10-11 08:51:53 -0500

    Delete tutorial node pages

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,8 @@ import 'home.dart';
 import 'package:flutter/material.dart';
 import 'drive-button.dart';
 import 'manual.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
-}
+void main() => runApp(const MyApp());
 
 /* */
 class MyApp extends StatelessWidget {
