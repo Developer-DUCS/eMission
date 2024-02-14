aa4ee0f647f789f9914a5554bd0ae5ad349fcc7d
Jacob Hutchinson <jacob.j.hutchinson.98@gmail.com>
2024-02-14 09:32:39 -0600

    Removed all files except for server

--- android/app/src/main/kotlin/com/example/first_flutter_app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package emission.dev
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-    // No additional code needed for a basic Flutter app
-}
--- lib/encryption.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-import 'package:crypto/crypto.dart';
-
-// Encrypts password with SHA256
-String encryptPassword(String password) {
-  final bytes = utf8.encode(password);
-  final hash = sha256.convert(bytes);
-  return hash.toString();
-}
